d356f42964786ebc9f4cd0d53bc9e540908929cb
daarunia <maximevaneygen@gmail.com>
2026-03-28 22:28:25 +0100

    init tests

---
+++ playwright.config.ts
@@ -0,0 +1,7 @@
+export default {
+  webServer: {
+    command: 'node scripts/dev-server.js',
+    port: 8080,
+    reuseExistingServer: true,
+  },
+};
---
+++ tests/app.spec.ts
@@ -0,0 +1,22 @@
+import { test as baseTest, expect, Browser, Page, BrowserContext } from "@playwright/test";
+
+let page: Page;
+let context: BrowserContext;
+
+baseTest.beforeAll(async ({ browser }) => {
+  context = await browser.newContext();
+  page = await context.newPage();
+
+  // Nav sur l'app
+  await page.goto("http://localhost:8080");
+  await expect(page.locator("#app")).toBeVisible();
+});
+
+baseTest.afterAll(async () => {
+  await page.close();
+  await context.close();
+});
+
+baseTest("app launches", async () => {
+  await expect(page).toHaveTitle(/NexTask/);
+});
