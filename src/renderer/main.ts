2b5467fbe3557f62dd567415067d826c66acd011
daarunia <maximevaneygen@gmail.com>
2026-07-25 19:07:37 +0200

    chore: bump dependencies

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -20,6 +20,8 @@ app.use(pinia)
 app.use(logger)
 app.use(router)
 app.use(PrimeVue, {
+  license:
+    'eyJpZCI6IjQ4MzgxMDQzLTY2MjctNGQ0ZC04M2MwLTkzNTEwMTg2NjM0ZSIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODQyMjkwNTAsImV4cCI6MTgxNTc2NTA1MH0.qVJvuNXG7ABnceENJ-N-CyWTFqegDmNA23La3zNAEVxL81AI-TJYx6gOrPJigHBaltNNnMhuW28uCSElaglrDA',
   theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } },
 })
 app.mount('#app')
